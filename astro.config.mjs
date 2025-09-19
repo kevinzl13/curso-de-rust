afcccc47e1db29b9724345f9c231f5a9cbfc442e
Kevin Carrion <86639616+kevinzl13@users.noreply.github.com>
2025-09-19 18:52:43 -0500

    deploy gh pahes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    devToolbar: {
-        enabled: false
-    }
+	devToolbar: {
+		enabled: false,
+	},
+	site: 'https://kevinzl13.github.io',
+	base: '/curso-de-rust',
 });
